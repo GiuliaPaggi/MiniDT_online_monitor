237332541a97063d12fa87b3fca904c697ffee10
GiuliaPaggi <79851597+GiuliaPaggi@users.noreply.github.com>
2022-05-17 20:14:48 +0200

    Added possibility to use simulated data file
    Entering -1 as run number the programs reads the simulated file while the tbo_bo_writer_simulator writes it

--- tbo_bo_reader.py
+++ tbo_bo_reader.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import time, os, sys
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
 
 
@@ -23,9 +24,7 @@ def ch_occupancy_plot(ax, channels, entries):
     ax.set_title(filename+datetime.now().strftime("%Y/%m/%d - %H:%M:%S"))
     plt.pause(.0001)
     plt.show()
-
-
-
+    
 
 
 # ----- run number can be passed as an argument from the terminal command line
@@ -33,13 +32,18 @@ def ch_occupancy_plot(ax, channels, entries):
 if len(sys.argv)>1:
     n_run = sys.argv[1] 
 else :
-    n_run = int( input('Error, no run number specified. Enter the run number and press the enter key: ') )
-    
-filename = "/home/gpaggi/dtupy/scripts/MiniDT_Runs/Run_" + str(n_run) + ".txt"             
+    n_run = int( input('Error, no run number specified. Enter the run number and press the enter key (to use the simulated data file, enter -1) :') )
+
+# ----- to use the file generated with the simulator use r_number = -1 -----
+    if n_run == -1: 
+        filename = "FakeRun.txt"
+    else : 
+        filename = "/home/gpaggi/dtupy/scripts/MiniDT_Runs/Run_" + str(n_run) + ".txt"             
 
 # ----- check if the file exists, close the program if it does not -----
 if os.path.exists(filename):
     f = open(filename, 'r')
+    print('Reading ' + filename )
 
 else:
     print("Error, output data file does not exists! Exiting...")
@@ -52,17 +56,21 @@ c = [119, 115,  96,  29,  32,  19,  34,  22, 142, 105, 137, 120, 124, 152, 149, 
 d = [ 40,  62,  73, 153, 148, 155, 156, 157, 159, 166,  61,  82,  77,  68, 146, 147]
 
 
+layer=[4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4,2,3,1]
+wire=[1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14,15,15,15,15,16,16,16,16]
+
+
 channel = range(64)
 pins = a+b+c+d
 
 # ----- set interactive mode so that pyplot.show() displays the figures and immediately returns -----
 plt.ion() 
+fig, ax = plt.subplots(2, 1, figsize = (25, 10))
 
-# ----- setup the histogram plot -----
-fig, ax = plt.subplots()
-ax.set_xticks([0, 8, 16, 24, 32, 40, 48, 56, 64], fontsize=6)
-ax.set_xlabel('Channels')
-ax.set_ylabel('Entries')
+# ----- set up 1d channel occupancy -----
+ax[0].set_xticks([0, 8, 16, 24, 32, 40, 48, 56, 64])
+ax[0].set_xlabel('Channels')
+ax[0].set_ylabel('Entries')
 
 entries = [0] *64 
 
@@ -89,8 +97,8 @@ try:
         
         # if reading fails, sleep 0.1s and set pointer back before the failed reading
         if not line:
-                    
-            time.sleep(.1)
+
+            time.sleep(.5)
             f.seek(where)
         # if the reading is successfull process the string
         else :
@@ -104,6 +112,8 @@ try:
                         # assign the channel to the pin and count the entries for each channel value
                         data_channel = pins.index(data_pin)
                         entries[data_channel] +=1
+
+                    
                 else: 
                     data_pins = -1
                 
@@ -114,7 +124,8 @@ try:
             #refresh plot if more than 1s is passed since previous one
             t2 = time.time()
             if t2-t1 > 1 :
-                ch_occupancy_plot(ax, channel, entries)
+                ch_occupancy_plot(ax[0], channel, entries)
+
                 # reset timer
                 t1 = t2 
         			 
@@ -122,7 +133,8 @@ try:
         
 except KeyboardInterrupt:
     print ('\nReading stopped.\n')
-        
+    f.close()
+
 
 
 
