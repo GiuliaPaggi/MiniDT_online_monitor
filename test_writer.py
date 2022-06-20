e7d59ecbd9d30ba9d94b32554fbf455798646122
GiuliaPaggi <79851597+GiuliaPaggi@users.noreply.github.com>
2022-06-20 21:59:23 +0200

    Fixed import name

--- test_writer.py
+++ test_writer.py
@@ -1,32 +1,32 @@
-import EVENT   
+import SORT   
 from hypothesis import given
 import hypothesis.strategies as st
 
 @given( st.lists(st.integers(), min_size=4, max_size=(4)))
 def test_lenlist(len_list):
-    output = EVENT.sort_layers(len_list)
+    output = SORT.sort_layers(len_list)
     assert len(output) == len(len_list)
     
     
 @given( st.lists(st.integers(), min_size=5)  )
 def test_longlist(t_list):
-    output = EVENT.sort_layers(t_list)
+    output = SORT.sort_layers(t_list)
     assert output == ValueError
     
 @given( st.lists(st.integers(), min_size=0, max_size=(3))  )
 def test_shortlist(t_list):
-    output = EVENT.sort_layers(t_list)
+    output = SORT.sort_layers(t_list)
     assert output == ValueError
 
 @given( st.lists(st.integers(), min_size=4, max_size=(4)))
 def test_sortedlist(srt_list):
     bx = [2, 5, 3, 7]
-    output = EVENT.sort_layers(bx)
+    output = SORT.sort_layers(bx)
     assert output == bx
     
 @given( st.lists(st.integers(), min_size=4, max_size=(4)))
 def test_sortlayers(bxlist):
-    output = EVENT.sort_layers(bxlist)
+    output = SORT.sort_layers(bxlist)
     L4= output[0]
     L3= output[2]
     L2= output[1]
